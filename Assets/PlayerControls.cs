e333ceb2b2b1dfee561769f47c4842bec1df89a6
TheRealWalrus <baranyferenc2@gmail.com>
2020-01-03 02:06:26 +0100

    first commit

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public KeyCode moveUp;
+    public KeyCode moveDown;
+    public float speed = 10f;
+
+    private Rigidbody2D rigidBody2D;
+
+    private void Awake()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(moveUp))
+        {
+            rigidBody2D.velocity = new Vector2(0, speed);
+        }
+        else if (Input.GetKey(moveDown))
+        {
+            rigidBody2D.velocity = new Vector2(0, -speed);
+        }
+        else
+        {
+            rigidBody2D.velocity = new Vector2();
+
+        }
+    }
+}
