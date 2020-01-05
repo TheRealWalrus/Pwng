99537289ca84804710a64d7b3fba47dc2099eaae
TheRealWalrus <baranyferenc2@gmail.com>
2020-01-06 00:01:57 +0100

    add Ball

---
+++ Assets/BallControl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+    public float forceMagnitude;
+
+    private void Awake()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+
+        //double radians = 2.4215435981750; WIP
+        //double radians = Math.PI;
+        double radians = UnityEngine.Random.Range((float)Math.PI * 0.75f, (float)Math.PI * 1.25f);
+        float randomDir = UnityEngine.Random.Range(0f, 1f);
+        Debug.Log(randomDir);
+
+        //if (randomDir < 0.5f)
+        //{
+        //    radians = UnityEngine.Random.Range((float) Math.PI * 1.75f, (float) Math.PI * 2.25f);
+        //    Debug.Log("right " + radians);
+        //}
+        //else
+        //{
+        //    radians = UnityEngine.Random.Range((float) Math.PI * 0.75f, (float) Math.PI * 1.25f);
+        //    Debug.Log("left " + radians);
+        //}
+
+        Vector2 direction = new Vector2((float)Math.Cos(radians), (float)Math.Sin(radians));
+        direction *= forceMagnitude;
+
+        rigidBody2D.AddForce(direction);
+    }
+}
--- Assets/GameSetup.cs
+++ Assets/GameSetup.cs
@@ -14,12 +14,13 @@ public class GameSetup : MonoBehaviour
     public Transform player2;
 
     public float wallSize;
+    public float paddleDistance;
 
     void Update()
     {
         // placed in update, to handle dynamic screen size changes
         topWall.size = new Vector2(mainCam.ScreenToWorldPoint(new Vector3(Screen.width, 0f)).x * 2f + wallSize * 2, wallSize);
-        topWall.transform.position = new Vector3 (0f, mainCam.ScreenToWorldPoint(new Vector3(0f, Screen.height)).y + wallSize / 2);
+        topWall.transform.position = new Vector3(0f, mainCam.ScreenToWorldPoint(new Vector3(0f, Screen.height)).y + wallSize / 2);
 
         bottomWall.size = new Vector2(mainCam.ScreenToWorldPoint(new Vector3(Screen.width, 0f)).x * 2f + wallSize * 2, wallSize);
         bottomWall.transform.position = new Vector3(0f, mainCam.ScreenToWorldPoint(new Vector3(0f, 0f)).y - wallSize / 2);
@@ -29,5 +30,11 @@ public class GameSetup : MonoBehaviour
 
         rightWall.size = new Vector2(wallSize, mainCam.ScreenToWorldPoint(new Vector3(0f, Screen.height)).y * 2);
         rightWall.transform.position = new Vector3(mainCam.ScreenToWorldPoint(new Vector3(Screen.width, 0f)).x + wallSize / 2, 0f);
+
+        float player1y = player1.transform.position.y;
+        player1.transform.position = new Vector3(mainCam.ScreenToWorldPoint(new Vector3(0f, 0f)).x + paddleDistance, player1y);
+
+        float player2y = player2.transform.position.y;
+        player2.transform.position = new Vector3(mainCam.ScreenToWorldPoint(new Vector3(Screen.width, 0f)).x - paddleDistance, player2y);
     }
 }
